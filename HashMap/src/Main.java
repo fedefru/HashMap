3097cbd9916c6eff5f8ee0eed1be40f4344d4d48
Federico Frutos <49793837+fedefru@users.noreply.github.com>
2019-10-23 09:06:11 -0300

    Add files via upload

--- HashMap/src/Main.java
+++ HashMap/src/Main.java
@@ -4,42 +4,23 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Persona fede = new Persona("Federico","Frutos","Los Andes 1521", 39113098,24,"Soltero");
-		Persona ariel = new Persona("Ariel","Vola","Guemes 1978", 2343234,20,"Casado con hijos");
+		Persona fede = new Persona("Federico","Frutos","Los Andes 1521", 39113098,24,"Soltero.");
+		Persona junior = new Persona("junior","mendoza","arroba", 3123123,23,"Soltero.");
+		Persona ariel = new Persona("ariel","mascapo","arrobaishon", 345345,23,"casado con hijos.");
 		
-		//Persona ariel = new Persona("Ariel","Vola","Guemes 1978", 32233123,20,"Soltero");
 		
-		//HashMap para traer el DNI
-		HashMap<String,Integer> dni = new HashMap<String, Integer>();
 		
-		dni.put(fede.getNombre(), fede.getDni());
-		dni.put(ariel.getNombre(), ariel.getDni());
+		//
 		
-		System.out.println("Dni fede: "+dni.get(fede.getNombre()));
-		System.out.println("Dni ariel: "+dni.get(ariel.getNombre()));
-		//HashMap para traer la Direccion
-		HashMap<String,String> direccion = new HashMap<String, String>();
+		//
+		HashMap<Object,Object> mapaPersona = new HashMap<Object, Object>();
 		
-		direccion.put(fede.getNombre(), fede.getDireccion());
-		direccion.put(ariel.getNombre(), ariel.getDireccion());
+		mapaPersona.put(fede.getNombre(), fede.getLista());
+		mapaPersona.put(junior.getNombre(),junior.getLista());
+		mapaPersona.put(ariel.getNombre(),ariel.getLista());
 		
-		System.out.println("Direccion fede: "+direccion.get(fede.getNombre()));
-		System.out.println("Direccion ariel: "+direccion.get(ariel.getNombre()));
 		
-		//HashMap para traer el Apellido
-		HashMap<String,String> apellido = new HashMap<String, String>();
-		
-		apellido.put(fede.getNombre(), fede.getApellido());
-		
-		System.out.println("Apellido: "+apellido.get(fede.getNombre()));
-		
-		//HashMap para traer la Edad
-		HashMap<String,Integer> edad = new HashMap<String, Integer>();
-		
-		edad.put(fede.getNombre(), fede.getEdad());
-		
-		System.out.println("Edad: "+edad.get(fede.getNombre()));
-		
+		mapaPersona.forEach((k, v) -> System.out.println("[Nombre] " + k + "  [Datos] " + v));
 		
 		
 		
--- HashMap/src/Persona.java
+++ HashMap/src/Persona.java
@@ -1,60 +1,37 @@
+import java.awt.List;
 
 public class Persona {
 	private String nombre;
-	private String apellido;
-	private String direccion;
-	private int dni;
-	private int edad;
-	private String estadoCivil; 
-	
-	public Persona(String nombre, String apellido, String direccion, int dni,int edad, String estadoCivil ) {
-		this.nombre=nombre;
-		this.apellido=apellido;
-		this.direccion=direccion;
-		this.dni=dni;
-		this.edad=edad;
-		this.estadoCivil=estadoCivil;
+
+	private List lista = new List();
+
+	public Persona(String nombre, String apellido, String direccion, int dni, int edad, String estadoCivil) {
+		this.nombre = nombre;
+		lista.add(apellido);
+		lista.add(direccion);
+		lista.add(String.valueOf(dni));
+		lista.add(String.valueOf(edad));
+		lista.add(estadoCivil);
 	}
 
+	public String getLista() {
+		String datos = " ";
+		for (int i = 0; i < lista.getItemCount(); i++)
+			datos = datos.concat(lista.getItem(i) + " ");
 
+		return datos;
+	}
 
-
-
-	public int getEdad() {
-		return edad;
-	}
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
-	public String getEstadoCivil() {
-		return estadoCivil;
-	}
-	public void setEstadoCivil(String estadoCivil) {
-		this.estadoCivil = estadoCivil;
-	}
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public int getDni() {
-		return dni;
-	}
-	public void setDni(int dni) {
-		this.dni = dni;
-	}
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
+
+	public void setLista(List lista) {
+		this.lista = lista;
 	}
 
 }
